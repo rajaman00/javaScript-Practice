bbf1a3c55988015bb72aa778703e67e22de9b656
rajaman00 <125030059+rajaman00@users.noreply.github.com>
2024-07-11 12:19:36 +0000

    String Discussion

---
+++ javascriprtBasics/05_string.js
@@ -0,0 +1,25 @@
+const name = "Raj";
+const repoCount = 10;
+
+//console.log(name + repoCount + " Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+ const gameName = new String("Sudoko")
+//  console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+//console.log(gameName.charAt(2));
+//console.log(gameName.indexOf('k'));
+
+const newStringOne = "    Kumar   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+console.log(url.replace('%20','-'))
+console.log(url.includes('hitesh'));
+
