a0e277a5040649d600d1876d9d9d3e086b506058
rajaman00 <125030059+rajaman00@users.noreply.github.com>
2024-07-11 06:34:38 +0000

    initial commit

---
+++ javascriprtBasics/02_Datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) //doesn't work , works in browsers
+
+let name = "raj"
+let age = "22"
+let isLoggedIn= false
+
+   /* primitive */
+//number => 2 to power 53
+//bigint---bahut jyada number ke liye
+//string=> ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+/*non primitive */
+//object
+
+
+
+
+
+
+console.log(typeof "Raj")
+
+// type of null is object
+
+
+
